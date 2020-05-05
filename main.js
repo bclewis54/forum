502d516e30915d429eb5f04bf6d317cfbea5ce9c
bclewis54 <lewisbradleychanning@gmail.com>
2020-05-04 21:58:41 -0400

    "First Commit"

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// your code here
+var newPost = document.createElement('p');
+var post = document.createTextNode('Test post');
+
+newPost.appendChild(post);
+
+document.getElementsByClassName('posts')[0].append(newPost);
